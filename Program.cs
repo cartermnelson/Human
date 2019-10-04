756843d2995e623a6c4c3585d5605c7a2e950434
Carter Nelson <carternelson05@gmail.com>
2019-10-04 10:02:56 -0700

    Wrote second constructor and Attack method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace Human
     {
         static void Main(string[] args)
         {
-            Human hum = new Human("poop");
+            Human hum = new Human("Sven");
+            Human hum2 = new Human("Sbeve", 4, 4, 4, 200);
+            Console.WriteLine(hum.Attack(hum2));
         }
     }
     class Human
@@ -16,7 +18,7 @@ namespace Human
         public int Strength;
         public int Intelligence;
         public int Dexterity;
-        private int health { get; }
+        private int health { get; set; }
 
         // add a public "getter" property to access health
 
@@ -43,6 +45,8 @@ namespace Human
         // Build Attack method
         public int Attack(Human target)
         {
+            target.health -= 5*Strength;
+            return target.health;
         }
     }
 
